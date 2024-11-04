db95b3693a49e9723386e4992f21066437a5adcd
Vlad Silin <vlad.silin.y@gmail.com>
2024-11-03 21:09:47 -0500

    Solve "Dutch National Flag Partition"

--- epi_judge_python/dutch_national_flag.py
+++ epi_judge_python/dutch_national_flag.py
@@ -9,7 +9,55 @@ RED, WHITE, BLUE = range(3)
 
 
 def dutch_flag_partition(pivot_index: int, A: List[int]) -> None:
-    # TODO - you fill in here.
+    pivot = A[pivot_index]
+    A[pivot_index], A[len(A) - 1] = A[len(A) - 1], A[pivot_index]
+
+    # The last element strictly smaller than the pivot
+    i = -1
+    # The last element which is strictly equal to the pivot
+    k = -1
+
+    # The index which will "find" the smaller elements
+    j = 0
+
+    # TODO: Add to notes (Useful approach to stepping through array problems)
+    # pivot = 2
+    #  [1, 2, 1, 2, 3]
+    #   i
+    # k
+    #      j
+    while j < len(A) - 1:
+        # The element is strictly LESS than the pivot
+        if A[j] < pivot:
+            # If an "equal" partition exists, 2 chained swaps
+            if k > -1:
+                A[j], A[k + 1] = A[k + 1], A[j]
+                A[i + 1], A[k + 1] = A[k + 1], A[i + 1]
+                i += 1
+                k += 1
+            # Otherwise, do the normal partition action
+            else:
+                A[j], A[i + 1] = A[i + 1], A[j]
+                i += 1
+        # The element is strictly EQUAL to the pivot
+        elif A[j] == pivot:
+            # If a "less" partition already exists but not yet an "equal" one, then create an "equal" one at the end
+            # of the "less" one.
+            if i > -1 and k < 0:
+                A[j], A[i + 1] = A[i + 1], A[j]
+                k = i + 1
+            else:
+                A[j], A[k + 1] = A[k + 1], A[j]
+                k += 1
+
+        j += 1
+
+    # Move the pivot from the end of the list to the correct place
+    if k > -1:
+        A[len(A) - 1], A[k + 1] = A[k + 1], A[len(A) - 1]
+    else:
+        A[len(A) - 1], A[i + 1] = A[i + 1], A[len(A) - 1]
+
     return
 
 
@@ -40,6 +88,9 @@ def dutch_flag_partition_wrapper(executor, A, pivot_idx):
 
 
 if __name__ == '__main__':
+    # test_list = [0, 0, 0, 0, 1, 1, 2, 1, 1]
+    # dutch_flag_partition(4, test_list)
+    # print(test_list)
     exit(
         generic_test.generic_test_main('dutch_national_flag.py',
                                        'dutch_national_flag.tsv',
