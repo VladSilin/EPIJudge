4f91eed3b01620bc3c33e0ad2c46d480084d85d3
Vlad Silin <vlad.silin.y@gmail.com>
2024-11-03 20:34:03 -0500

    Solve "Buy and Sell Stock Once"

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -4,8 +4,28 @@ from test_framework import generic_test
 
 
 def buy_and_sell_stock_once(prices: List[float]) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    if not prices:
+        return 0
+    elif len(prices) == 1:
+        return 0
+
+    # [310, 315, 275, 295, 260, 270, 290, 230, 255, 250]
+    buy = prices[0]
+    sell = prices[1]
+
+    profit = max(0.0, sell - buy)
+
+    j = 1
+    while j < len(prices):
+        if prices[j] < buy:
+            buy = prices[j]
+        else:
+            if prices[j] - buy > profit:
+                profit = max(0.0, prices[j] - buy)
+
+        j += 1
+
+    return profit
 
 
 if __name__ == '__main__':
