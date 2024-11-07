bb235eb930144ad4eff1ebf88445d21a2a9d1d20
Vlad Silin <vlad.silin.y@gmail.com>
2024-11-06 21:07:59 -0500

    Solve "K Largest Values in a BST"

--- epi_judge_python/k_largest_values_in_bst.py
+++ epi_judge_python/k_largest_values_in_bst.py
@@ -5,8 +5,20 @@ from test_framework import generic_test, test_utils
 
 
 def find_k_largest_in_bst(tree: BstNode, k: int) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    # TODO: Add to notes (ascending order in-order traversal)
+    def _in_order_reverse(_tree: BstNode, _storage: [BstNode]):
+        if not _tree or len(_storage) >= k:
+            return
+
+        _in_order_reverse(_tree.right, _storage)
+        if len(_storage) < k:
+            storage.append(_tree.data)
+            _in_order_reverse(_tree.left, _storage)
+
+    storage = []
+    _in_order_reverse(tree, storage)
+
+    return storage
 
 
 if __name__ == '__main__':
