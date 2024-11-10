0031bc79925003aae4b90d9fb52bb6798436c2e5
Vlad Silin <vlad.silin.y@gmail.com>
2024-11-10 16:57:38 -0500

    Solve "x to the Power of y"

--- epi_judge_python/power_x_y.py
+++ epi_judge_python/power_x_y.py
@@ -1,9 +1,26 @@
 from test_framework import generic_test
 
 
+def power2(x: float, y: int) -> float:
+    result = 1
+    for _ in range(1, abs(y) + 1):
+        result *= (x if y > 0 else 1 / x)
+
+    return result
+
+
 def power(x: float, y: int) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    exp = y
+
+    if y < 0:
+        x, exp = 1 / x, -exp
+
+    if exp == 0:
+        return 1
+
+    halved_exp_result = power(x, exp >> 1)
+
+    return halved_exp_result * halved_exp_result * x if exp & 1 else halved_exp_result * halved_exp_result
 
 
 if __name__ == '__main__':
