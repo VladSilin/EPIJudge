17157d38953dea0d011a3838c61afc37ca6777ec
Vlad Silin <vlad.silin.y@gmail.com>
2024-11-10 16:18:35 -0500

    Solve "Sample Offline Data"

--- epi_judge_python/offline_sampling.py
+++ epi_judge_python/offline_sampling.py
@@ -1,4 +1,5 @@
 import functools
+import random
 from typing import List
 
 from test_framework import generic_test
@@ -8,8 +9,24 @@ from test_framework.random_sequence_checker import (
 from test_framework.test_utils import enable_executor_hook
 
 
+def random_sampling2(k: int, A: List[int]) -> None:
+    init_num_elements = len(A)
+    for i in range(init_num_elements - k):
+        s = random.randint(0, len(A) - 1)
+        print('Len:', len(A))
+        print('s:', s)
+        A.remove(A[s])
+        print(A)
+
+    return
+
+
 def random_sampling(k: int, A: List[int]) -> None:
-    # TODO - you fill in here.
+    for i in range(k):
+        random_index = random.randint(i, len(A) - 1)
+
+        A[i], A[random_index] = A[random_index], A[i]
+
     return
 
 
@@ -41,7 +58,12 @@ def random_sampling_wrapper(executor, k, A):
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main('offline_sampling.py',
-                                       'offline_sampling.tsv',
-                                       random_sampling_wrapper))
+    sample_size = 3
+    samples = [1, 2, 3, 4, 5, 6, 7]
+    random_sampling(sample_size, samples)
+    print(samples[:sample_size])
+
+    # exit(
+    #     generic_test.generic_test_main('offline_sampling.py',
+    #                                    'offline_sampling.tsv',
+    #                                    random_sampling_wrapper))
