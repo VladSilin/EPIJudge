ac6516de0e2e8464bdc5b9fc03c355b47afe47fe
Vlad Silin <vlad.silin.y@gmail.com>
2024-11-12 16:49:48 -0500

    Solve "Merge Many Sorted Arrays"

--- epi_judge_python/sorted_lists_merge.py
+++ epi_judge_python/sorted_lists_merge.py
@@ -3,11 +3,38 @@ from typing import Optional
 from list_node import ListNode
 from test_framework import generic_test
 
+'''
+NOTE: When representing a list as a node, this node points to the head and should remain pointing to the head.
+
+A SEPARATE pointer should always be created which can be updated to iterate through the list.
+'''
+
 
 def merge_two_sorted_lists(L1: Optional[ListNode],
                            L2: Optional[ListNode]) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    # L1: 1 -> 3 -> 4 -> 7 -> 9
+    #          p
+    # L2: 1 -> 2 -> 8 -> 11 -> 13
+    #          q
+
+    output_list_dummy_head = output_pointer = ListNode()
+    p1, p2 = L1, L2
+
+    while p1 and p2:
+        if p1.data <= p2.data:
+            output_pointer.next = ListNode(p1.data)
+            output_pointer = output_pointer.next
+
+            p1 = p1.next
+        else:
+            output_pointer.next = ListNode(p2.data)
+            output_pointer = output_pointer.next
+
+            p2 = p2.next
+
+    output_pointer.next = p1 or p2
+
+    return output_list_dummy_head.next
 
 
 if __name__ == '__main__':
