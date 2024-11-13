ca33f5b20c6e9b3a6b6d7b3c0415a7eadc78c1c5
Vlad Silin <vlad.silin.y@gmail.com>
2024-11-12 20:40:29 -0500

    Solve "Binary Tree Depth Order Traversal"

--- epi_judge_python/tree_level_order.py
+++ epi_judge_python/tree_level_order.py
@@ -3,10 +3,39 @@ from typing import List
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
+#   314
+#   /  \
+#  6    6
+# / \  / \
+# 1 3  4 7
+#
+# [[314]]
+
 
 def binary_tree_depth_order(tree: BinaryTreeNode) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    if not tree:
+        return []
+
+    result = []
+    visit_queue = [tree]
+
+    while visit_queue:
+        result.append([node.data for node in visit_queue])
+
+        # NOTE: Equivalent imperative version (note the similar order of nested for's)
+        # new_visit_queue = []
+        # for node_to_visit in visit_queue:
+        #     for child in (node_to_visit.left, node_to_visit.right):
+        #         if child:
+        #             new_visit_queue.append(child)
+        # visit_queue = new_visit_queue
+
+        # NOTE: Basically "extracting" what would/could be in the "kernel" of the nested loops to the
+        # place where "child" is
+        visit_queue = [child for node_to_visit in visit_queue
+                       for child in (node_to_visit.left, node_to_visit.right) if child]
+
+    return result
 
 
 if __name__ == '__main__':
