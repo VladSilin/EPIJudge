10147be4235f1b8c741e1db103fcac2b875a35d7
Vlad Silin <vlad.silin.y@gmail.com>
2025-01-29 12:54:46 -0500

    Solve "Compute a Random Permutation"

--- epi_judge_python/random_subset.py
+++ epi_judge_python/random_subset.py
@@ -1,5 +1,6 @@
 import functools
 from typing import List
+import random
 
 from test_framework import generic_test
 from test_framework.random_sequence_checker import (
@@ -7,10 +8,44 @@ from test_framework.random_sequence_checker import (
     compute_combination_idx, run_func_with_retries)
 from test_framework.test_utils import enable_executor_hook
 
+# TODO: Add to notes (random permutations not straightforward reasoning from book, also Coupon Collector's Problem)
+# The total number of ways in which we can choose
+# the elements to swap is 3 ** 3 = 27 and all are equally likely. Since 27 is not divisibleby 6, some
+# permutations correspond to more ways than others, so not all permutations are equally likely.
+#
+# TODO: Add to notes (permutations vs combinations, repeated elements vs not)
+# [1, 2, 3]
 
+# [1, 2, 3] swap 0, 0
+  # [1, 3, 2]
+# [2, 1, 3] swap 0, 1
+  # [2, 3, 1]
+# [3, 2, 1] swap 0, 2
+  # [3, 1, 2]
+
+# Input:
+# n: int, number of elements for random selection; k: int, size of random subset
+
+# Output:
+# a random subset of the numbers [0, 1, ..., n - 1] (all subsets should be equally likely)
+
+# Notes / Assumptions:
+
+# Example:
+
+# TODO: Add to notes (use a map to emulate a mapping of a subset of array elements)
 def random_subset(n: int, k: int) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    changed_elements = {}
+
+    for slot in range(k):
+        random_index = random.randrange(slot, n)
+        random_index_value = changed_elements.get(random_index, random_index)
+
+        slot_value = changed_elements.get(slot, slot)
+        changed_elements[random_index] = slot_value
+        changed_elements[slot] = random_index_value
+
+    return [changed_elements[i] for i in range(k)]
 
 
 @enable_executor_hook
@@ -33,6 +68,13 @@ def random_subset_wrapper(executor, n, k):
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main('random_subset.py', 'random_subset.tsv',
-                                       random_subset_wrapper))
+    n_input = 100
+    k_input = 4
+
+    result = random_subset(n_input, k_input)
+
+    print(result)
+
+    #exit(
+    #    generic_test.generic_test_main('random_subset.py', 'random_subset.tsv',
+    #                                   random_subset_wrapper))
