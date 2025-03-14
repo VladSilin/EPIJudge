bf030f04436fb4fd459a11277516f7d864bc0640
Vlad Silin <vlad.silin.y@gmail.com>
2025-03-13 23:17:35 -0400

    Solve "Compute the Successor"

--- epi_judge_python/successor_in_tree.py
+++ epi_judge_python/successor_in_tree.py
@@ -6,10 +6,39 @@ from test_framework import generic_test
 from test_framework.binary_tree_utils import must_find_node
 from test_framework.test_utils import enable_executor_hook
 
+# Inputs
+# - node: BinaryTreeNode; A node of a binary tree
 
+# Output
+# - node: BinaryTreeNode; The successor of the given node in an inorder traversal
+
+# Notes / Assumptions
+# - Nodes have parent pointers
+
+# Example
+
+# Brute Force Outline
+# - Traverse parent pointers back to root
+# - Do inorder traversal, store the nodes (O(N) time, O(N) space, N nodes)
+# - Iterate through list of nodes, return the successor
+
+# Optimized Outline
+# - Assume the given node is the root of a tree
+#     - Would the successor be left or right of it?
+#         - Neither, could be above
 def find_successor(node: BinaryTreeNode) -> Optional[BinaryTreeNode]:
-    # TODO - you fill in here.
-    return None
+    if node.right:
+        cur = node.right
+        while cur.left:
+            cur = cur.left
+
+        return cur
+
+    cur = node
+    while cur.parent and cur.parent.right is cur:
+        cur = cur.parent
+
+    return cur.parent
 
 
 @enable_executor_hook
