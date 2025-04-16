1c14990d0a166c7722862c338803d50def4ae9d9
Vlad Silin <vlad.silin.y@gmail.com>
2025-04-16 09:09:10 -0400

    Solve "Sort an Almost-Sorted Array"

--- epi_judge_python/sort_almost_sorted_array.py
+++ epi_judge_python/sort_almost_sorted_array.py
@@ -1,12 +1,66 @@
+import heapq
 from typing import Iterator, List
 
+import itertools
+
+from setuptools.dist import sequence
+
 from test_framework import generic_test
+
+# Inputs:
+# sequence: Iterator[int], a long sequence of numbers
+# k: The largest distance a num. is away from its sorted position
+
+# Outputs:
+# result: List[int], a sorted list
+
+# Notes / Assumptions:
+
+# Brute Force:
+# - Perform an insertion sort into a new array (O(n^2))
+# -
+def sort_approximately_sorted_array0(sequence: Iterator[int],
+                                    k: int) -> List[int]:
+    result = []
+    min_heap = []
+
+    count = 0
+    while True:
+        try:
+            elem = next(sequence)
+            if count <= k:
+                heapq.heappush(min_heap, elem)
+                k += 1
+            else:
+                smallest = heapq.heappushpop(min_heap, elem)
+                result.append(smallest)
+        except StopIteration:
+            break
+
+    while min_heap:
+        smallest = heapq.heappop(min_heap)
+        result.append(smallest)
+
+    return result
 
 
 def sort_approximately_sorted_array(sequence: Iterator[int],
                                     k: int) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    result = []
+    min_heap = []
+
+    for x in itertools.islice(sequence, k):
+        heapq.heappush(min_heap, x)
+
+    for x in sequence:
+        smallest = heapq.heappushpop(min_heap, x)
+        result.append(smallest)
+
+    while min_heap:
+        smallest = heapq.heappop(min_heap)
+        result.append(smallest)
+
+    return result
 
 
 def sort_approximately_sorted_array_wrapper(sequence, k):
