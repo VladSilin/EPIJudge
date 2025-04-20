80a449e46412149bf5804dbf32d67e726a92170d
Vlad Silin <vlad.silin.y@gmail.com>
2025-04-20 14:35:52 -0400

    Solve "Do Lists Overlap, No Cycles"

--- epi_judge_python/do_lists_overlap.py
+++ epi_judge_python/do_lists_overlap.py
@@ -6,10 +6,64 @@ from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
+# Input:
+# l0: ListNode, head of a linked list
+# l1: ListNode, head of another linked list
+
+# Output
+# shared_node: ListNode, the first node common to both lists
+
+# Notes / Assumptions:
+
+# Example:
+# - Check if the next node of both lists is the
+# 1 -> N
+# 1 -> N
+
+# 1 -> 2 -> N
+#      ^
+# 1 ---|
+
+# Outline:
+
+def overlapping_lists0(l0: ListNode, l1: ListNode) -> Optional[ListNode]:
+    if l0 is None or l1 is None:
+        return None
+    # TODO - you fill in here.
+    l0_nodes = set()
+    while l0 is not None:
+        l0_nodes.add(id(l0))
+        l0 = l0.next
+
+    while l1 is not None:
+        if id(l1) in l0_nodes:
+            return l1
+
+        l1 = l1.next
+
+    return None
 
 def overlapping_lists(l0: ListNode, l1: ListNode) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    def length(L):
+        length = 0
+        while L is not None:
+            length += 1
+            L = L.next
+
+        return length
+
+    l0_len, l1_len = length(l0), length(l1)
+    if l0_len > l1_len:
+        # l1 is always the longer list
+        l0, l1 = l1, l0
+
+    for _ in range(abs(l0_len - l1_len)):
+        l1 = l1.next
+
+    while l0 and l1 and l0 is not l1:
+        l0, l1 = l0.next, l1.next
+
+    return l0
 
 
 @enable_executor_hook
