2d7ad107d4de7e2b0266561755278ca583f1ef53
Vlad Silin <vlad.silin.y@gmail.com>
2025-04-20 19:32:28 -0400

    Solve "Delete Kth Last Element From List"

--- epi_judge_python/delete_kth_last_from_list.py
+++ epi_judge_python/delete_kth_last_from_list.py
@@ -3,11 +3,35 @@ from typing import Optional
 from list_node import ListNode
 from test_framework import generic_test
 
+# Input:
+# L: ListNode, head of linked list
+# k: int, which node from last to return
+
+# Output:
+# kth_last_node: ListNode, the kth last node of the list
+
+# Notes / Assumptions:
+# - Cannot use more than a few words of extra storage
+
+# Example:
+
+# Outline:
 
 # Assumes L has at least k nodes, deletes the k-th last node in L.
 def remove_kth_last(L: ListNode, k: int) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    dummy_head = ListNode(0, L)
+
+    first = dummy_head.next
+    for _ in range(k):
+        first = first.next
+
+    second = dummy_head
+    while first:
+        first, second = first.next, second.next
+
+    second.next = second.next.next
+
+    return dummy_head.next
 
 
 if __name__ == '__main__':
