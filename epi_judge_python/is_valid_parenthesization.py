5f47a39fad423b6c75b91727307acca425274e1e
Vlad Silin <vlad.silin.y@gmail.com>
2025-04-23 19:18:21 -0400

    Solve "Is Valid Parenthesization"

--- epi_judge_python/is_valid_parenthesization.py
+++ epi_judge_python/is_valid_parenthesization.py
@@ -1,12 +1,44 @@
 from test_framework import generic_test
 
+# Input:
+# s: str, a string containing {,},(,),[,]
 
+# Output:
+# is_well_formed: bool, do all the parens. match?
+
+# Notes / Assumptions:
+
+# Example:
+# [()[]{()()}]
+#
+# [[(]
+
+# Outline:
 def is_well_formed(s: str) -> bool:
-    # TODO - you fill in here.
-    return True
+    open_to_close_map = {
+        '{': '}',
+        '(': ')',
+        '[': ']',
+    }
+
+    stack = []
+    for c in s:
+        if c in open_to_close_map:
+            stack.append(c)
+        else:
+            if not stack or open_to_close_map[stack.pop()] != c:
+                return False
+
+    return not stack
 
 
 if __name__ == '__main__':
+    #test = '[()[]{())}'
+
+    #well_formed = is_well_formed(test)
+
+    #print(well_formed)
+
     exit(
         generic_test.generic_test_main('is_valid_parenthesization.py',
                                        'is_valid_parenthesization.tsv',
