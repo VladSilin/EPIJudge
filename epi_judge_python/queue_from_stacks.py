70a09400bc338487768c3a89219bbe0d122ada76
Vlad Silin <vlad.silin.y@gmail.com>
2025-04-23 22:30:05 -0400

    Solve "Implement a queue using stacks"

--- epi_judge_python/queue_from_stacks.py
+++ epi_judge_python/queue_from_stacks.py
@@ -3,13 +3,21 @@ from test_framework.test_failure import TestFailure
 
 
 class Queue:
+    def __init__(self):
+        self._input_stack, self._output_stack = [], []
+
     def enqueue(self, x: int) -> None:
-        # TODO - you fill in here.
-        return
+        self._input_stack.append(x)
 
     def dequeue(self) -> int:
-        # TODO - you fill in here.
-        return 0
+        if not self._output_stack:
+            while self._input_stack:
+                self._output_stack.append(self._input_stack.pop())
+
+        if not self._output_stack:
+            raise IndexError('Dequeue from empty queue')
+
+        return self._output_stack.pop()
 
 
 def queue_tester(ops):
@@ -33,6 +41,14 @@ def queue_tester(ops):
 
 
 if __name__ == '__main__':
+    #q = Queue()
+    #q.enqueue(1)
+    #q.enqueue(2)
+    #q.enqueue(3)
+
+    #r1, r2, r3 = q.dequeue(), q.dequeue(), q.dequeue()
+    #print(r1, r2, r3)
+
     exit(
         generic_test.generic_test_main('queue_from_stacks.py',
                                        'queue_from_stacks.tsv', queue_tester))
